44f410ee2f9e16bead4569cfdc3f18293fb11bfa
NourhanNabil <nourhan.nabill@outlook.com>
2022-02-11 03:37:49 +0200

    updates for rest-frameworks

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('post/<post_id>', views.postDetails, name='post-details'),
+    #rest_framework URLs.
+    path('api-all', views.api_all_users, name='api-all'),
+    path('api-one/<user_id>', views.api_one_user, name='api-one'),
+    path('api-add', views.api_add_user, name='api-add'),
+    path('api-edit/<user_id>', views.api_edit_user, name='api-edit'),
+    path('api-del/<user_id>', views.api_del_user, name='api-del')
+
  ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from multiprocessing import context
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Member,Post,ForbiddenWord,Category
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import MemberSerializer
 
 def home(request):
     all_posts=Post.objects.all()
@@ -16,3 +18,32 @@ def postDetails(request,post_id):
     context={'post':one_post}
     return render(request,'blog/post_details.html',context)
 
+#rest Framework views here.
+@api_view(['GET'])
+def api_all_users(request):
+    all_users = Member.objects.all()
+    user_ser = MemberSerializer(all_users, many=True)
+    return Response(user_ser.data)
+@api_view(['GET'])
+def api_one_user(request, user_id):
+    user = Member.objects.get(id=user_id)
+    user_ser = MemberSerializer(user, many=False)
+    return Response(user_ser.data)
+@api_view(['POST'])
+def api_add_user(request):
+    user_ser = MemberSerializer(data=request.data)
+    if user_ser.is_valid():
+        user_ser.save()
+        return redirect('api-all')
+@api_view(['POST'])
+def api_edit_user(request, user_id):
+    user= Member.objects.get(id=user_id)
+    user_ser = MemberSerializer(data=request.data, instance=user)
+    if user_ser.is_valid():
+        user_ser.save()
+        return redirect('api-all')
+@api_view(['DELETE'])
+def api_del_user(request, user_id):
+    user= Member.objects.get(id=user_id)
+    user.delete()
+    return Response('User Deleted successfully!')
