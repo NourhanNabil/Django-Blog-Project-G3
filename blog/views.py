8f3e7d5903be56aafe22389004924644ced8bb8b
sohybnaim <sohya50@gmail.com>
2022-02-16 16:52:22 +0200

    test pull

--- blog/views.py
+++ blog/views.py
@@ -100,8 +100,10 @@ def register_view(request):
 
 
 # like post view
+# like post view
 def LikeView(request, pk):
-    post = get_object_or_404(Post, id=request.POST.get("post_id"))
+    # post_id as the submit button name
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
     liked = False
     if post.likes.filter(id=request.user.id).exists():
         post.likes.remove(request.user)
@@ -110,7 +112,6 @@ def LikeView(request, pk):
         post.likes.add(request.user)
         liked = True
     return HttpResponseRedirect(reverse("post-details", args=[str(pk)]))
-
 
 def search_bar(request):
     if request.method == "POST":
