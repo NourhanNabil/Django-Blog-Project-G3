07bc8d4a3ee1089eac99827652a57aaefcc55530
NourhanNabil <nourhan.nabill@outlook.com>
2022-02-17 21:50:11 +0200

    alter category to be unique value

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,8 @@ from ckeditor.fields import RichTextField
 
 
 class Category(models.Model):
-    category = models.CharField(max_length=50)
-    subscribes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='category_subscribes', null=True)
+    category = models.CharField(max_length=50,unique=True)
+    subscribes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='category_subscribes')
 
     def total_subscribes(self):
         return self.subscribes.count()
